38d49a424f0d484b24db1abe57df747076f78c20
Ryland Donovan <32310318+RrylandD@users.noreply.github.com>
2025-02-24 20:29:56 -0500

    Update cors allowed domains

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -50,8 +50,8 @@ export default async function handler(
 ) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.thenottawaarearesidentsassociation.com');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
